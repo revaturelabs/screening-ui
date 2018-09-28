42bef8f5787f8568201ba5f8c51e4cce226faf72
fearlesspandas <landonrenzullo@gmail.com>
2018-09-28 13:25:01 -0400

    app-alerts

---
+++ src/app/components/alerts/alerts.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { AlertsService } from '../services/alerts.service';
+import { NotificationsService } from 'angular2-notifications-lite';
+
+@Component({
+  moduleId: module.id.toString(),
+  selector: 'app-alerts',
+  templateUrl: './alerts.component.html',
+  styleUrls: ['./alerts.component.css'],
+})
+export class AlertsComponent implements OnInit {
+
+  message: any;
+
+  /**
+   * global config for notification
+   */
+  public options = {
+    position: ['bottom', 'left'],
+    timeOut: 2500,
+    maxStack: 10,
+    maxLength: 36,
+    lastOnBottom: true,
+    showProgressBar: false,
+    preventDuplicates: true,
+  };
+
+  constructor(private alertService: AlertsService,
+    private notif: NotificationsService) { }
+
+  ngOnInit() {
+    this.showNotif();
+  }
+
+  /**
+   * display success/error notif
+   */
+  showNotif() {
+    this.alertService.getMessage().subscribe(message => {
+      this.message = message;
+      if (this.message.type === 'success') {
+        this.notif.success('Success', this.message.text);
+      } else {
+        this.notif.error('Error', this.message.text);
+      }
+    });
+  }
+}
