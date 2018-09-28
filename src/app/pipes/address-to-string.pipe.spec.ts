8bfdd2dbbb95a958fa3b1fc318d440f54ddde392
fearlesspandas <landonrenzullo@gmail.com>
2018-09-28 10:38:29 -0400

    rmvd uneccessary files

--- src/app/pipes/address-to-string.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddressToStringPipe } from './address-to-string.pipe';
-
-describe('AddressToStringPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AddressToStringPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
