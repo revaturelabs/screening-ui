71408d52ebf17a8ba9b04265550e194a8edde11e
Alan Sato <sam.alan.sato@gmail.com>
2018-10-01 10:06:17 -0400

    newline at end of file

--- src/app/components/alerts/alerts.component.spec.ts
+++ src/app/components/alerts/alerts.component.spec.ts
@@ -44,4 +44,50 @@
 //       // component.showNotif();
 //       expect(component.message.text).toBe('this is a test');
 //   }));
+// })import { inject, ComponentFixture, TestBed } from '@angular/core/testing';
+// import { AlertsComponent } from './alerts.component';
+// import { AlertsService } from '../../services/alert-service/alerts.service';
+// import { NotificationsService } from 'angular2-notifications-lite';
+// import { Dependencies } from '../../caliber.test.module';
+
+// /**
+//  * Author: Jordan Young
+//  */
+
+// describe('AlertsComponent', () => {
+//   let component: AlertsComponent;
+//   let fixture: ComponentFixture<AlertsComponent>;
+
+//   // set up
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [AlertsComponent],
+//       imports: [],
+//       providers: [AlertsService, NotificationsService]
+//     });
+//   });
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AlertsComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   // tests for showNotif()
+
+//   // case 1: error alert
+//   it('should show error', inject([AlertsService, NotificationsService],
+//     (aService: AlertsService, nService: NotificationsService) => {
+//       aService.error('this is a test');
+//       // component.showNotif();
+//       expect(component.message.text).toBe('this is a test');
+//   }));
+
+//   // case 2: success alert
+//   it('should show success', inject([AlertsService, NotificationsService],
+//     (aService: AlertsService, nService: NotificationsService) => {
+//       aService.success('this is a test');
+//       // component.showNotif();
+//       expect(component.message.text).toBe('this is a test');
+//   }));
 // });
