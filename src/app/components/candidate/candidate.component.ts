3c6dc3ae60d03ad0115a292d54205dedbba03cdc
Alan Sato <sam.alan.sato@gmail.com>
2018-10-01 15:01:24 -0400

    modal created for candidate settings

--- src/app/components/candidate/candidate.component.ts
+++ src/app/components/candidate/candidate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-candidate',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  open(content) {
+    this.modalService.open(content, { windowClass: 'fixed-modal' });
   }
 
 }
