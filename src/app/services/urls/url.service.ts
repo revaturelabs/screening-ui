7ef4877de41524c871f1f5ef2554e81546409f27
fearlesspandas <landonrenzullo@gmail.com>
2018-10-01 16:41:33 -0400

    datatesting

--- src/app/services/urls/url.service.ts
+++ src/app/services/urls/url.service.ts
@@ -3,7 +3,7 @@ import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class UrlService {
-  public readonly context: string =  environment.gambitContext;
+  public readonly context: string =  environment.context;
 
   apiCurrentBatchesLineChart = this.context + 'all/reports/dashboard';
 
@@ -85,7 +85,7 @@ export class UrlService {
    *
    * @author Pedro De Los Reyes | 1803-USF-MAR26 | Wezley Singleton
    */
-  private bucketEndpoint = '/buckets';
+  private bucketEndpoint = '/bucket';
   bucket = {
     getAllBuckets: () => `${this.context + this.bucketEndpoint}`,
     getBucketById: (bucketId: number) => `${this.context + this.bucketEndpoint}/${bucketId}`,
@@ -207,7 +207,7 @@ export class UrlService {
    *
    * @author Pedro De Los Reyes | 1803-USF-MAR26 | Wezley Singleton
    */
-  private questionEndpoint = (this.context + '/questions');
+  private questionEndpoint = (this.context + '/question');
   question = {
     postQuestion: () => `${this.questionEndpoint}`,
     putQuestion: () => `${this.questionEndpoint}`,
