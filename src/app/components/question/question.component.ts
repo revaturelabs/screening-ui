f96036f8ec2a4493d1764a0673fb7ea53200813d
Joseph Dao <josephvdao@gmail.com>
2018-10-17 14:30:31 -0400

    allowed user to add question to bucket/category

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -150,6 +150,8 @@ export class QuestionComponent implements OnInit {
         this.question.sampleAnswer3 = this.sampleAnswers[2];
         this.question.sampleAnswer4 = this.sampleAnswers[3];
         this.question.sampleAnswer5 = this.sampleAnswers[4];
+        this.question.isActive = true;
+        this.question.bucket = this.currentBucket;
         this.questionService.createNewQuestion(this.question).subscribe();
         this.savedSuccessfully();
       }
