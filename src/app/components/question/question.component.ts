56133537ec78772fc4f64af6ac7ce1eb843a93cc
Joseph Dao <josephvdao@gmail.com>
2018-10-18 14:16:59 -0400

    fixed some tests in the question component test file so they'd pass

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -11,7 +11,7 @@ import { AlertsService } from '../../services/alert-service/alerts.service';
 
 @Component({
   selector: 'app-question',
-  templateUrl: './question.component.html',
+  template: './question.component.html',
   styleUrls: ['./question.component.css'],
   animations: [
     trigger('move', [
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,8 +16,8 @@
  * @author Pedro De Los Reyes | 1803-USF-MAR26 | Wezley Singleton
  */
 
-  const adminContext = 'http://ec2-54-210-14-237.compute-1.amazonaws.com:8181';
-  const screeningContext = 'http://ec2-54-210-14-237.compute-1.amazonaws.com:8183';
+  const adminContext = 'http://http://10.252.1.159:9001/';
+  const screeningContext = 'http://10.252.1.159:9001/';
 
 const bam = '';
 
@@ -28,8 +28,8 @@ export const environment = {
   /** Assets endpoints for like pictures  */
   assets: 'http://52.87.205.55:8086/angular/assets/',
 
-  adminContext: 'http://ec2-54-210-14-237.compute-1.amazonaws.com:8181',
-  screeningContext: 'http://ec2-54-210-14-237.compute-1.amazonaws.com:8183',
+  adminContext: 'http://10.252.1.159:9001/',
+  screeningContext: 'http://10.252.1.159:9001/',
   bam: bam,
   url: 'http://54.166.255.85:8085',
   msurl: 'http://34.227.178.103:',
