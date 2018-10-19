5061fd9f038b9fd9df39b97f5de729b077817035
Joseph Dao <josephvdao@gmail.com>
2018-10-18 20:06:42 -0400

    added tests for bucket service methods

--- src/app/services/buckets/buckets.service.spec.ts
+++ src/app/services/buckets/buckets.service.spec.ts
@@ -1,23 +1,76 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { BucketsService } from './buckets.service';
-import { HttpClientModule } from '@angular/common/http';
-import { UrlService } from '../urls/url.service';
+import { TestBed } from "@angular/core/testing";
+import { BucketsService } from "./buckets.service";
+import { UrlService } from "../urls/url.service";
+import { HttpClientModule } from "@angular/common/http";
 
 describe('BucketsService', () => {
+  let service: BucketsService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
+    TestBed.configureTestingModule({ 
       imports: [
         HttpClientModule,
       ],
       providers: [
         BucketsService,
         UrlService,
-      ]
-    });
+      ]});
   });
 
-  it('should be created', inject([BucketsService], (service: BucketsService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should set a bucket', () => {
+    let service = TestBed.get(BucketsService);
+
+    let bucket = {
+      bucketId: 0,
+      bucketDescription: 'description',
+      isActive: true
+    };
+
+    service.setBucket(bucket);
+    expect(service.currentBucket).toEqual(bucket);
+  });
+
+  it('should get the current bucket', () => {
+    let service = TestBed.get(BucketsService);
+    
+    let bucket = {
+      bucketId: 0,
+      bucketDescription: 'description',
+      isActive: true
+    };
+    
+    service.setBucket(bucket);
+    let currentBucket = service.getCurrentBucket();
+    expect(currentBucket).toEqual(bucket);
+  });
+
+  it('should set the current bucket\'s description', () => {
+    let service = TestBed.get(BucketsService);
+
+    let bucket = {
+      bucketId: 0,
+      bucketDescription: 'description',
+      isActive: true
+    };
+
+    service.setBucket(bucket);
+    let description = 'new description';
+    service.setDescription(description);
+    expect(service.currentBucket.bucketDescription).toEqual(description);
+  });
+
+  it('should get the current bucket\'s description', () => {
+    let service = TestBed.get(BucketsService);
+
+    let bucket = {
+      bucketId: 0,
+      bucketDescription: 'description',
+      isActive: true
+    };
+
+    service.setBucket(bucket);
+    let description = service.getDescription();
+    expect(description).toEqual(bucket.bucketDescription);
+  });
+
 });
