38e22fed2ef0cda7bf3345e6f5d2670cd33f0f39
Joseph Dao <josephvdao@gmail.com>
2018-10-24 11:56:08 -0400

    updated karma config file to enable code coverage report

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,9 +9,13 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    preprocessors: {
+      '**/app/*.js': 'coverage',
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
