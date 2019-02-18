07f0fa4f0e9e0cd718ae4de044a2b59d29749a21
David Carlson <david.carlson2012@gmail.com>
2019-02-18 14:16:32 -0600

    Fixing hardest-questions styling

--- src/app/components/reports/hardest-questions/hardest-questions.component.ts
+++ src/app/components/reports/hardest-questions/hardest-questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hardest-questions',
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hardest-questions.component.css']
 })
 export class HardestQuestionsComponent implements OnInit {
-  hardestQuestions: string[] = [];
+  @Input() hardestQuestions: string[] = [
+    "What are the sublanguages of SQL?",
+    "What are the primary JDBC interfaces?",
+    "What is the difference between statements and prepared statements and why would we prefer one over the other?",
+    "What values are falsey in JavaScript?",
+    "What is SOAP?"
+   ];
   constructor() { }
 
   ngOnInit() {
