81c15119bbe47d0511b038c8f8abc137a91dbf04
Will Scott <williamtscott0@gmail.com>
2019-04-09 11:21:06 -0500

    create question and ansers for admin

--- src/app/components/admin-tab/admin-tab.component.ts
+++ src/app/components/admin-tab/admin-tab.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
 
 @Component({
+  
   selector: 'app-admin-tab',
   templateUrl: './admin-tab.component.html',
   styleUrls: ['./admin-tab.component.css']
 })
 export class AdminTabComponent implements OnInit {
-
+  @ViewChild('createQuestionForm') formValues;
+  notifier: string;
+  //public items:Array<string> =['Java Applied','Hibernate','SQL','Angular','Spring AoP','JavaScript','Java Concepts','SOAP','JDBC','JDBC']
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitForm(form: NgForm) {
+ 
+    this.notifier = "Your Question was created!";
+    this.formValues.resetForm();
+  }
 }
