617df49b192dba5b41d99e5ee1115f76ea926a84
John Vasgird <38052305+chec13@users.noreply.github.com>
2019-04-09 14:19:02 -0500

    merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -40,7 +40,7 @@ export const routes: Routes = [
   },
   
   {
-    path: ' ',
+    path: '',
     canActivate: [AuthenticationService],
     //component: CandidatesScreeningListComponent,
     children: [
