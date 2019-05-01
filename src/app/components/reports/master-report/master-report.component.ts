63598a162c3e9cc8ca558da2cca9d97ec2084333
danarday <danjarday@gmail.com>
2019-05-01 14:13:11 -0400

    Weeks slider updated

--- src/app/components/reports/master-report/master-report.component.ts
+++ src/app/components/reports/master-report/master-report.component.ts
@@ -11,15 +11,17 @@ import { ReportCacheService } from 'src/app/services/reports/report-cache.servic
 export class MasterReportComponent implements OnInit {
   reportData: ReportData;
   currentSearchTerm: string = '';
-  currentWeeksValue: Array<number> = [0, 26];
+  currentWeeksValue: Array<number> = [0, 52];
 
   //Replace this with a function to populate the current dates automatically
   dates: Array<string> = ["2019-01-01","2018-12-25","2018-12-18","2018-12-11","2018-12-04","2018-12-04","2018-11-27","2018-11-20","2018-11-13","2018-11-06","2018-10-30","2018-10-23","2018-10-16",
-                          "2018-10-09","2018-10-02","2018-09-25","2018-09-18","2018-09-18","2018-09-11","2018-09-04","2018-08-28","2018-08-21","2018-08-14","2018-08-07","2018-07-31","2018-07-24"
+                          "2018-10-09","2018-10-02","2018-09-25","2018-09-18","2018-09-18","2018-09-11","2018-09-04","2018-08-28","2018-08-21","2018-08-14","2018-08-07","2018-07-31","2018-07-24",
+                          "2018-07-17","2018-07-10","2018-07-03","2018-06-26","2018-06-19","2018-06-12","2018-06-05","2018-05-29","2018-05-22","2018-05-15","2018-05-08","2018-05-01","2018-04-24",
+                          "2018-04-17","2018-04-10","2018-04-03","2018-03-27","2018-03-20","2018-03-13","2018-03-06","2018-02-27","2018-02-20","2018-02-13","2018-02-06","2018-01-30","2018-01-23"
                           ]
   
   startDate: string = "2015-01-01";
-  endDate: string = "2019-01-01"
+  endDate: string = "2019-01-01";
 
   constructor(
     private reportService: ReportService,
@@ -28,6 +30,8 @@ export class MasterReportComponent implements OnInit {
   ngOnInit() {
     this.reportCache.getAllScreenerDataByWeeks(this.startDate, this.endDate)
       .subscribe(data => this.handleNewReportData(data));
+
+      //populateDates()
   }
   handleNewReportData(data: ReportData) {
     //console.log(data);
@@ -71,4 +75,9 @@ export class MasterReportComponent implements OnInit {
     }
   }
 
+  populateDate(currentDate, weeksBack){
+    //while (n < weeksBack){
+      //currDate -= 7 days;
+    //}
+  //}
 }
