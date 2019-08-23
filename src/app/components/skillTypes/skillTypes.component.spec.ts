428e8589ee21e07d3e53da76c6a95d5df066cdd5
Dylan Waser <waserd@etsu.edu>
2019-08-23 15:44:45 -0400

    working on testing Track Toggle

--- src/app/components/skillTypes/skillTypes.component.spec.ts
+++ src/app/components/skillTypes/skillTypes.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { async, TestBed, ComponentFixture, fakeAsync } from '@angular/core/testing';
 import { SkillTypesComponent } from './skillTypes.component';
 import { NgbModule, NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, FormBuilder } from '@angular/forms';
@@ -9,11 +9,20 @@ import { UrlService } from 'src/app/services/urls/url.service';
 import { AlertsService } from 'src/app/services/alert-service/alerts.service';
 import { SkillTypesService } from 'src/app/services/skill-types/skill-types.service';
 import { SkillTypeBucketService } from 'src/app/services/skillTypeBucketLookup/skill-type-bucket.service';
+import { SKILLTYPES } from 'src/app/mock-data/mock-skillTypes';
+import { SkillType } from 'src/app/entities/SkillType';
+import { of } from 'rxjs';
 
 describe('skillTypesComponent', () => {
 
     let component: SkillTypesComponent;
     let fixture: ComponentFixture<SkillTypesComponent>;
+    let fakeSkillTypeService = jasmine.createSpyObj('SkillTypesService', ['updateSkillType']);
+    let mockSkillType:SkillType = {
+        skillTypeId: 52,
+        title: 'Java EE/Microservices',
+        active: true
+    }
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -33,5 +42,30 @@ describe('skillTypesComponent', () => {
     it(`should create`, (done: DoneFn) => {
         expect(component).toBeTruthy();
         done();
+    });
+
+    it('should change track status', (done: DoneFn) => {
+        //make a spy of whatever service is retrieving track infromation from database on init component.oninit
+        //pass in fake track array
+        //check if DOM is updated
+        //check if DOM is updated dynamically
+        //strucutral directive
+        //once gotten dummy data make sure its listed correctly
+        //identify toggle button
+        //declaration inside test to c if toggle button works
+        //it('should change question status', () => {
+            //     component.changeQuestionStatus(QUESTIONS[0]);
+            //     expect(QUESTIONS[0].isActive).toBe(false);
+            //     component.changeQuestionStatus(QUESTIONS[0]);
+            //     expect(QUESTIONS[0].isActive).toBe(true);
+            //   });
+
+            // Create a fake TwainService object with a `getQuote()` spy
+            //const twainService = jasmine.createSpyObj('TwainService', ['getQuote']);
+            // Make the spy return a synchronous Observable with the test data
+            //getQuoteSpy = twainService.getQuote.and.returnValue( of(testQuote) );       
+        const skillTypeChanged = fakeSkillTypeService.updateSkillType.and.returnValue(of(mockSkillType));
+        expect(mockSkillType.active).toBe(false)
+        
     });
 });
