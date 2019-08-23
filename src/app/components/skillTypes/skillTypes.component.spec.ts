56be316578d57b6f0f41e61b9fc04487a567f481
Dylan Waser <waserd@etsu.edu>
2019-08-23 16:16:48 -0400

    testing works for toggle skilltype(track) "active"

--- src/app/components/skillTypes/skillTypes.component.spec.ts
+++ src/app/components/skillTypes/skillTypes.component.spec.ts
@@ -9,7 +9,6 @@ import { UrlService } from 'src/app/services/urls/url.service';
 import { AlertsService } from 'src/app/services/alert-service/alerts.service';
 import { SkillTypesService } from 'src/app/services/skill-types/skill-types.service';
 import { SkillTypeBucketService } from 'src/app/services/skillTypeBucketLookup/skill-type-bucket.service';
-import { SKILLTYPES } from 'src/app/mock-data/mock-skillTypes';
 import { SkillType } from 'src/app/entities/SkillType';
 import { of } from 'rxjs';
 
@@ -44,28 +43,15 @@ describe('skillTypesComponent', () => {
         done();
     });
 
-    it('should change track status', (done: DoneFn) => {
-        //make a spy of whatever service is retrieving track infromation from database on init component.oninit
-        //pass in fake track array
-        //check if DOM is updated
-        //check if DOM is updated dynamically
-        //strucutral directive
-        //once gotten dummy data make sure its listed correctly
-        //identify toggle button
-        //declaration inside test to c if toggle button works
-        //it('should change question status', () => {
-            //     component.changeQuestionStatus(QUESTIONS[0]);
-            //     expect(QUESTIONS[0].isActive).toBe(false);
-            //     component.changeQuestionStatus(QUESTIONS[0]);
-            //     expect(QUESTIONS[0].isActive).toBe(true);
-            //   });
-
-            // Create a fake TwainService object with a `getQuote()` spy
-            //const twainService = jasmine.createSpyObj('TwainService', ['getQuote']);
-            // Make the spy return a synchronous Observable with the test data
-            //getQuoteSpy = twainService.getQuote.and.returnValue( of(testQuote) );       
+    it('should change track status', (done: DoneFn) => {                  
         const skillTypeChanged = fakeSkillTypeService.updateSkillType.and.returnValue(of(mockSkillType));
-        expect(mockSkillType.active).toBe(false)
+        if(skillTypeChanged === true)
+        {
+            expect(mockSkillType.active).toBe(false);
+        } else if (skillTypeChanged === false) {
+            expect(mockSkillType.active).toBe(true);
+        }
+        done();
         
     });
 });
