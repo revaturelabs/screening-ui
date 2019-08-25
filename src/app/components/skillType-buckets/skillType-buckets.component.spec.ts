01be2314a5d0692fa82a91fcabc72356f0dac80a
github_Hekuta <rotcehlugo@gmail.com>
2019-08-25 14:27:46 -0400

    Bucket Test

--- src/app/components/skillType-buckets/skillType-buckets.component.spec.ts
+++ src/app/components/skillType-buckets/skillType-buckets.component.spec.ts
@@ -1,7 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { BucketsService } from '../../services/buckets/buckets.service';
 import { SkillTypeBucketsComponent } from './skillType-buckets.component';
 import { FormsModule } from '@angular/forms';
@@ -10,21 +8,20 @@ import { AlertsService } from 'src/app/services/alert-service/alerts.service';
 import { HttpModule } from '@angular/http';
 import { Buckets } from 'aws-sdk/clients/s3';
 import { Bucket } from 'src/app/entities/Bucket';
-
-
 import { of } from 'rxjs';
-
 import { Observable } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { UrlService } from 'src/app/services/urls/url.service';
-
 describe('SkillTypeBucketsComponent', () => {
-
 let fixture:ComponentFixture<SkillTypeBucketsComponent>;
 let component:SkillTypeBucketsComponent;
 let fakeBucketService:BucketsService;
-let fakeBukets:Bucket[]=[null];
-
+let mockbucket:Bucket={
+  bucketId: 1,
+  bucketDescription:"Hi",
+  isActive:true
+}
+let fakeBukets:Bucket[]=[mockbucket];
 beforeEach(()=>{
   TestBed.configureTestingModule({
     imports:[RouterTestingModule,FormsModule,HttpModule,HttpClientModule],
@@ -35,17 +32,17 @@ beforeEach(()=>{
   component=fixture.componentInstance;
   fakeBucketService=TestBed.get(BucketsService);
 });
-
-
 it(`Checking if all buckets were returned`,()=>{
    spyOn(fakeBucketService,`getAllBuckets`)
    .and.returnValues(of(fakeBukets));
   
-})
-
-
-
-
-
-
 });
+it('Checking if bucket is created', () => {
+  spyOn(fakeBucketService, 'createNewBucket')
+  .and.returnValues(of(fakeBukets[0]));
+})
+});
+
+
+
+
