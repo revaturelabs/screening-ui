fd8416314e11872f454c522f6c625dfea0b10b84
David_Juarez <djuarez1003@gmail.com>
2019-10-09 10:19:52 -0400

    got mock questions loading

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -8,6 +8,8 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
 import { BucketsService } from '../../services/buckets/buckets.service';
 import { AlertsService } from '../../services/alert-service/alerts.service';
 
+import { QUESTIONS } from '../../mock-data/mock-questions';
+
 
 @Component({
   selector: 'app-question',
@@ -59,8 +61,9 @@ export class QuestionComponent implements OnInit {
   ngOnInit() {
     this.currentBucket = this.bucketService.getCurrentBucket();
     this.question = new Question();
-    this.sampleAnswers = [this.question.sampleAnswer1,this.question.sampleAnswer2,this.question.sampleAnswer3,this.question.sampleAnswer4,this.question.sampleAnswer5];
-    this.updateQuestions();
+    this.questions = QUESTIONS;
+    // this.sampleAnswers = [this.question.sampleAnswer1,this.question.sampleAnswer2,this.question.sampleAnswer3,this.question.sampleAnswer4,this.question.sampleAnswer5];
+    // this.updateQuestions();
   }
 
   /**
--- src/app/components/skillType-buckets/skillType-buckets.component.ts
+++ src/app/components/skillType-buckets/skillType-buckets.component.ts
@@ -9,6 +9,10 @@ import { BucketFilterPipe } from '../../pipes/skillType-buckets.filter';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { AlertsService } from '../../services/alert-service/alerts.service';
 
+import { QUESTIONS } from '../../mock-data/mock-questions';
+import { BUCKETS } from '../../mock-data/mock-buckets';
+
+
 
 @Component({
   selector: 'app-skill-type-buckets',
@@ -20,6 +24,7 @@ export class SkillTypeBucketsComponent implements OnInit {
 
   /** variable to hold an array of 'Bucket' entities */
   buckets: Bucket[];
+  bucket: Bucket;
   /** variable to hold bucket being edited */
   currBucket: Bucket;
   /** variable to hold new bucket being created  */
@@ -41,10 +46,11 @@ export class SkillTypeBucketsComponent implements OnInit {
   }
 
   getBuckets(): void {
-    this.bucketService.getAllBuckets().subscribe(buckets => {
-      this.buckets = buckets;
-      this.buckets.sort(this.compare);
-    });
+      this.buckets = BUCKETS;
+    // this.bucketService.getAllBuckets().subscribe(buckets => {
+    //   this.buckets = buckets;
+    //   this.buckets.sort(this.compare);
+    // });
   }
 
   /** used to compare buckets Array to sort it based on status */
