c947932d92505b353e4d24fd6733a6b175d4f6ad
erchiggins <emilyrchiggins@gmail.com>
2019-10-24 16:42:06 -0400

    linting app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {RouterTestingModule} from '@angular/router/testing'
+import {RouterTestingModule} from '@angular/router/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -22,5 +22,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('screening-ui-gen');
   }));
-  
 });
