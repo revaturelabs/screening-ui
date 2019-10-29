894b0bdb7732f12c1ac47018b5accb7dc11379ed
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-10-29 13:09:49 -0400

    Bucket and Bucket to Category Type

--- src/app/entities/Weight.ts
+++ src/app/entities/Weight.ts
@@ -1,9 +1,9 @@
-import { Bucket } from './Category';
+import { Category } from './Category';
 import { SkillType } from './SkillType';
 
 export class Weight {
     weightId: number;
     weightValue: number;
     skillType: SkillType;
-    bucket: Bucket;
+    category: Category;
 }
