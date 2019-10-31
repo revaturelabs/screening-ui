c4a56bf2b403d7a0d5e51439ccf5d2c3c2dba96f
Mohammad Ahmar <ahmar.mohammad77@gmail.com>
2019-10-31 13:56:40 -0400

    final models

--- src/app/entities/Candidate.ts
+++ src/app/entities/Candidate.ts
@@ -2,9 +2,14 @@ export class Candidate {
     candidateId: number;
     resourceId: number;
     name: string;
+    email: string; //
     phoneNumber: string;
+    skypeId: string; // 
+    profileUrl: string;//
     recruiterName: string;
     college: string;
     degree: string;
     major: string;
+    techScreenerName: string;//
+    
 }
--- src/app/entities/Category.ts
+++ src/app/entities/Category.ts
@@ -1,5 +1,7 @@
 export class Category {
     categoryId: number;
     categoryDescription: string;
-    isActive = true;
+ //   isActive = true; //old 
+    isActive: boolean; //new
+    
 }
